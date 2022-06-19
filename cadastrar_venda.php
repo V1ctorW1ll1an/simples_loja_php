1806180d097579286b5c55c70ea9d16ffc2faedc
Victor Willian <victorwillian1012@gmail.com>
2022-06-19 00:16:29 -0300

    html cadastrar vendas

--- cadastrar_venda.php
+++ cadastrar_venda.php
@@ -34,7 +34,41 @@
 
   </div>
   <main class="container mt-5">
-    <h1>cadastrar venda</h1>
+    <section class="title">
+      <h1 class="mb-5">Cadastro de vendas</h1>
+
+      <form>
+        <div class="mb-3">
+          <label for="valor_parcial" class="form-label">Valor parcial</label>
+          <input id="valor_parcial" class="form-control" type="text" placeholder="Insira o valor em R$">
+        </div>
+        <div class="mb-3">
+          <label for="valor_desconto" class="form-label">Valor desconto</label>
+          <input id="valor_desconto" class="form-control" type="text" placeholder="Insira o valor em R$">
+        </div>
+        <div class="mb-3">
+          <label for="valor_acrescimo" class="form-label">Valor de acrescimo</label>
+          <input id="valor_acrescimo" class="form-control" type="text" placeholder="Insira o valor em R$">
+        </div>
+        <div class="mb-3">
+          <label for="cliente" class="form-label">Selecione um cliente</label>
+          <select id="cliente" class="form-select">
+            <option selected>Cliente</option>
+            <option value="1">Joao exemplo</option>
+            <option value="2">Maria exemplo</option>
+            <option value="3">Joaquim exemplo</option>
+          </select>
+        </div>
+        <fieldset disabled>
+          <div class="mb-3">
+            <label for="valor_total" class="form-label">Valor total</label>
+            <input id="valor_total" class="form-control" type="text" value="" placeholder="R$">
+          </div>
+        </fieldset>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+      </form>
+
+    </section>
   </main>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
