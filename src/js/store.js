e4e0c7e78d3df92dbc36762800d698ec4becb02c
Victor Willian <victorwillian1012@gmail.com>
2022-06-19 01:01:01 -0300

    calculando valor total

--- src/js/store.js
+++ src/js/store.js
@@ -2,4 +2,23 @@
     const valor_parcial = document.getElementById("valor_parcial");
     const valor_desconto = document.getElementById("valor_desconto");
     const valor_acrescimo = document.getElementById("valor_acrescimo");
+    const valor_total = document.getElementById("valor_total");
+
+    const calcTotalValue = (e) => {
+        const valor_parcial_value = valor_parcial.value
+            ? parseFloat(valor_parcial.value)
+            : 0;
+        const valor_desconto_value = valor_desconto.value
+            ? parseFloat(valor_desconto.value)
+            : 0;
+        const valor_acrescimo_value = valor_acrescimo.value
+            ? parseFloat(valor_acrescimo.value)
+            : 0;
+        valor_total.value =
+            valor_parcial_value - valor_desconto_value + valor_acrescimo_value;
+    };
+
+    [valor_parcial, valor_desconto, valor_acrescimo].map((ipt) =>
+        ipt.addEventListener("input", calcTotalValue)
+    );
 })();
