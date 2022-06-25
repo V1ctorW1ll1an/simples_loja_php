43cffdf67436fc972e19bb4dee49b22b11c7c091
victor willian <victorwillian1012@gmail.com>
2022-06-25 10:55:37 -0300

    add docker compose

--- src/config.php
+++ src/config.php
@@ -1,6 +1,12 @@
 <?php
 
-$mysql = new mysqli("localhost", "root", "", "Loja", 80);
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "Loja";
+$port = 80;
+
+$mysql = new mysqli("db", "root", "root", "Loja", 3306);
 
 $mysql->set_charset("utf8");
 
